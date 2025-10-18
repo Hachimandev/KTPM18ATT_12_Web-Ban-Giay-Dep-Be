75f5a8a546f942524a21831567b2a40a4252d06f
unknown <22640121.quyen@student.iuh.edu.vn>
2025-10-18 16:54:31 +0700

    tao be

---
+++ src/main/java/com/fit/web_ban_giay_dep_be/WebBanGiayDepBeApplication.java
@@ -0,0 +1,13 @@
+package com.fit.web_ban_giay_dep_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanGiayDepBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBanGiayDepBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fit/web_ban_giay_dep_be/WebBanGiayDepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.web_ban_giay_dep_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanGiayDepBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
