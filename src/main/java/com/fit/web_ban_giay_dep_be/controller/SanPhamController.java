48227502e97c3c23467940ce52ce747d070f593a
unknown <22640121.quyen@student.iuh.edu.vn>
2025-10-28 17:08:45 +0700

    add CRUD sanpham controller

--- src/main/java/com/fit/web_ban_giay_dep_be/controller/SanPhamController.java
+++ src/main/java/com/fit/web_ban_giay_dep_be/controller/SanPhamController.java
@@ -1,4 +1,53 @@
 package com.fit.web_ban_giay_dep_be.controller;
 
+import com.fit.web_ban_giay_dep_be.entity.SanPham;
+import com.fit.web_ban_giay_dep_be.service.SanPhamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sanpham")
 public class SanPhamController {
+
+    @Autowired
+    private SanPhamService sanPhamService;
+
+
+    @GetMapping
+    public ResponseEntity<List<SanPham>> getAllSanPham() {
+        return ResponseEntity.ok(sanPhamService.getAllSanPham());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SanPham> getSanPhamById(@PathVariable String id) {
+        return sanPhamService.getSanPhamById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<SanPham> createSanPham(@RequestBody SanPham sanPham) {
+        return ResponseEntity.ok(sanPhamService.addSanPham(sanPham));
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<SanPham> updateSanPham(@PathVariable String id, @RequestBody SanPham sanPham) {
+        return ResponseEntity.ok(sanPhamService.updateSanPham(id, sanPham));
+    }
+
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteSanPham(@PathVariable String id) {
+        sanPhamService.deleteSanPham(id);
+        return ResponseEntity.noContent().build();
+    }
 }
