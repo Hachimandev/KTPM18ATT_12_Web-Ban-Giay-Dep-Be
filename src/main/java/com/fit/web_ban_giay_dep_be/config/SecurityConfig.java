3965a78177d87f54f3faac929d4fb459dcfa88e6
unknown <22640121.quyen@student.iuh.edu.vn>
2025-10-28 17:14:16 +0700

    phan quyen, test ok

--- src/main/java/com/fit/web_ban_giay_dep_be/config/SecurityConfig.java
+++ src/main/java/com/fit/web_ban_giay_dep_be/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/api/sanpham", "/api/sanpham/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
