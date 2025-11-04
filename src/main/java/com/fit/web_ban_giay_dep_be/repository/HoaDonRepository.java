44633c495d059764669fa67c5d9e6e603e647798
Lưu Ngọc Cao Sơn <156668941+luungoccaoson@users.noreply.github.com>
2025-11-04 10:48:54 +0700

    Thêm repository, service, controller crud cho hóa đơn

--- src/main/java/com/fit/web_ban_giay_dep_be/repository/HoaDonRepository.java
+++ src/main/java/com/fit/web_ban_giay_dep_be/repository/HoaDonRepository.java
@@ -1,8 +1,9 @@
 package com.fit.web_ban_giay_dep_be.repository;
 
-
 import com.fit.web_ban_giay_dep_be.entity.HoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HoaDonRepository extends JpaRepository<HoaDon, String> {
 }
+
+
