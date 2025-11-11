3aca97e68f69cf6087d7f24eae8d023cd8f5b6d6
unknown <22640121.quyen@student.iuh.edu.vn>
2025-11-11 16:29:35 +0700

    add logic ve san pham va cart

---
+++ src/main/java/com/fit/web_ban_giay_dep_be/service/ChiTietSanPhamService.java
@@ -0,0 +1,18 @@
+package com.fit.web_ban_giay_dep_be.service;
+
+import com.fit.web_ban_giay_dep_be.entity.ChiTietSanPham;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ChiTietSanPhamService {
+    List<ChiTietSanPham> getAllChiTietSanPham();
+
+    Optional<ChiTietSanPham> getChiTietSanPhamById(String id);
+
+    ChiTietSanPham addChiTietSanPham(ChiTietSanPham chiTietSanPham);
+
+    ChiTietSanPham updateChiTietSanPham(String id, ChiTietSanPham chiTietSanPham);
+
+    void deleteChiTietSanPham(String id);
+}
